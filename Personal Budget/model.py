7988a63cdb7cc116418c88fdeeefbaafb3f9a300
sambogrub <137734917+sambogrub@users.noreply.github.com>
2025-01-16 11:18:36 -0600

    More work on the models
    Setting up basic getters and setters for the model attributes

--- Personal Budget/model.py
+++ Personal Budget/model.py
@@ -5,10 +5,10 @@ from datetime import datetime
 class Transaction:
     def __init__(self, date: datetime,
                  amount: float,
-                 account: int,
-                 category: str=None,
+                 account: int=None, #account id number matching database account number
+                 category: int=None, #category id number matcing database category number
                  description: str=None,
-                 id: int=None,
+                 id: int=None, #id number matching database id number
                  type: str=None
                  ):
         self._id = id
@@ -19,6 +19,14 @@ class Transaction:
         self._category = category
         self._description = description
 
+    @property
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, date_: datetime):
+        self._date = date_
+
     def determine_type(self):
         return 'income' if self.amount >= 0 else 'expense'
     
@@ -26,18 +34,82 @@ class Transaction:
     def type(self):
         return self._type
     
+    @type.setter
+    def type(self, label: str):
+        self._type = label
+
+    @property
+    def amount(self):
+        return self._amount
+    
+    @amount.setter
+    def amount(self, amount_: float):
+        self._amount = amount_
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, category_: int):
+        self._category = category_
+
+    @property
+    def account(self):
+        return self._account
+    
+    @account.setter
+    def account(self, account_: int):
+        self._account = account_
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, description_: str):
+        self._description = description_
+
+    @description.deleter
+    def description(self):
+        self.description = None    
+    
 
 class Account:
     def __init__(self,
                  name: str,
                  type: str,
                  id: int=None,
-                 balance:int = 0
+                 balance: int=0
                  ):
         self._id = id
         self._name = name
         self._type = type
         self._balance = balance
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name_):
+        self._name = name_
+
+    @property
+    def type(self):
+        return self._type
+    
+    @type.setter
+    def type(self, type_):
+        self._type = type_
+
+    @property
+    def balance(self):
+        return self._balance
+    
+    @balance.setter
+    def balance(self, balance_):
+        self.balance = balance_
 
 
 class Category:
@@ -49,6 +121,8 @@ class Category:
         self._id = id
         self._name = name
         self._parent_id = parent_id
+
+    
 
 
 class BudgetCategory:
